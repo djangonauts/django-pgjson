c26cefd4689c93e6e562bcf55cdd24cef6563a1c
Andrey Antukh <niwi@niwi.be>
2014-04-18 21:49:09 +0200

    Increment version to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ PostgreSQL json field support for Django.
 
 setup(
     name = "django-pgjson",
-    version = '0.1.0',
+    version = '0.1.1',
     url = 'https://github.com/niwibe/djorm-pgjson',
     license = 'BSD',
     platforms = ['OS Independent'],
