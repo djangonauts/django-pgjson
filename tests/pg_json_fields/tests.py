ab0af53d85aa6309f91ca51ea57f3525f51da0a2
David Fischer <david.fischer.ch@gmail.com>
2014-11-27 16:50:18 +0100

    Use specialized assertion methods

--- tests/pg_json_fields/tests.py
+++ tests/pg_json_fields/tests.py
@@ -23,28 +23,28 @@ class JsonFieldTests(TestCase):
         instance1 = TextModelWithDefault.objects.create()
         instance2 = TextModelWithDefault.objects.get(pk=instance1.pk)
 
-        self.assertEqual(instance1.data, {})
-        self.assertEqual(instance2.data, {})
+        self.assertDictEqual(instance1.data, {})
+        self.assertDictEqual(instance2.data, {})
 
     def test_empty_create(self):
         instance = self.model_class.objects.create(data={})
         instance = self.model_class.objects.get(pk=instance.pk)
-        self.assertEqual(instance.data, {})
+        self.assertDictEqual(instance.data, {})
 
     def test_unicode(self):
         obj = self.model_class.objects.create(data={"list": ["Fóö", "Пример", "test"]})
         obj = self.model_class.objects.get(pk=obj.pk)
-        self.assertEqual(obj.data["list"][1], "Пример")
+        self.assertListEqual(obj.data["list"], ["Fóö", "Пример", "test"])
 
     def test_array_serialization(self):
         obj = self.model_class.objects.create(data=["Fóö", "Пример", "test"])
         obj = self.model_class.objects.get(pk=obj.pk)
-        self.assertEqual(obj.data[1], "Пример")
+        self.assertListEqual(obj.data, ["Fóö", "Пример", "test"])
 
     def test_primitives_bool(self):
         obj = self.model_class.objects.create(data=True)
         obj = self.model_class.objects.get(pk=obj.pk)
-        self.assertEqual(obj.data, True)
+        self.assertTrue(obj.data)
 
     def test_primitives_str(self):
         obj = self.model_class.objects.create(data="Fóö")
@@ -54,7 +54,7 @@ class JsonFieldTests(TestCase):
     def test_primitives_str_valid_json(self):
         obj = self.model_class.objects.create(data='["Fóö", 3.1415]')
         obj = self.model_class.objects.get(pk=obj.pk)
-        self.assertEqual(obj.data, ["Fóö", 3.1415])
+        self.assertListEqual(obj.data, ["Fóö", 3.1415])
 
     def test_primitives_int(self):
         obj = self.model_class.objects.create(data=3)
@@ -69,7 +69,7 @@ class JsonFieldTests(TestCase):
     def test_primitives_null(self):
         obj = self.model_class.objects.create(data=None)
         obj = self.model_class.objects.get(pk=obj.pk)
-        self.assertEqual(obj.data, None)
+        self.assertIsNone(obj.data)
 
     def test_value_to_string_serializes_correctly(self):
         obj = self.model_class.objects.create(data={"a": 1})
